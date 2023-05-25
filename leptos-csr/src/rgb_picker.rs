ba960689e9810c3f28a3f49446b7a78c5c55753e
Ke7in <kevinwezenter@gmail.com>
2023-05-25 01:32:37 -0400

    refactored rgb_picker following Leptos guide 3.3
    refactored rgb_picker component, following Leptos Guide 3.3 Components and Props.

--- leptos-csr/src/rgb_picker.rs
+++ leptos-csr/src/rgb_picker.rs
@@ -2,136 +2,100 @@ use leptos::*;
 
 #[component]
 pub fn RgbPicker(cx: Scope) -> impl IntoView {
-    let (count_r, set_count_r) = create_signal(cx, 10);
-    let (count_g, set_count_g) = create_signal(cx, 10);
-    let (count_b, set_count_b) = create_signal(cx, 10);
-    let (input_r, set_input_r) = create_signal(cx, "".to_string());
-    let (input_g, set_input_g) = create_signal(cx, "".to_string());
-    let (input_b, set_input_b) = create_signal(cx, "".to_string());
+    let (value_r, set_value_r) = create_signal(cx, 10);
+    let (value_g, set_value_g) = create_signal(cx, 10);
+    let (value_b, set_value_b) = create_signal(cx, 10);
 
     view! { cx,
-        <div
-            style:background-color=move || format!("rgb({}, {}, {})", count_r(), count_g(), count_b())
-        >
         <div>
-            <p>"This is the blended values of Red Green and Blue"</p>
-        </div>
-        <div
-            style:background-color=move || format!("rgb({}, 0, 0", count_r())
-        >
-            <button // - R
-                on:click=move |_| {
-                    set_count_r.update(|n| *n-=1);
-                }
+            <div
+                style:background-color=move || format!("rgb({}, {}, {})", value_r(), value_g(), value_b())
             >
-                "Less Red"
-            </button>
-            {count_r}
-            <button // + R
-                on:click=move |_| {
-                    set_count_r.update(|n| *n+=1);
-                }
+                <div>
+                    <p>"This is the blended values of Red Green and Blue"</p>
+                </div>
+                <div>
+                    <p>"..."</p>
+                </div>
+            </div>
+            <div
+                style:background-color=move || format!("rgb({}, 0, 0", value_r())
             >
-                "More Red"
-            </button>
-            <progress
-                max="255"
-                value=count_r
-            />
-            <div>
-                <input type="text"
-                    on:input=move |ev| {
-                        set_input_r(event_target_value(&ev));
-                    }
-                >
-                </input>
-                <button
-                    on:click=move |_| {
-                        set_count_r(input_r.get_untracked().parse::<i32>().ok().unwrap_or_default())
-                    }
-                >
-                    "Set Red"
-                </button>
+                <RgbInput
+                    color="Red".to_string()
+                    value=value_r
+                    set_value=set_value_r
+                />
+            </div>
+            <div
+                style:background-color=move || format!("rgb(0, {}, 0", value_g())
+            >
+                <RgbInput
+                    color="Green".to_string()
+                    value=value_g
+                    set_value=set_value_g
+                />
+            </div>
+            <div
+                style:background-color=move || format!("rgb(0, 0, {}", value_b())
+            >
+                <RgbInput
+                    color="Blue".to_string()
+                    value=value_b
+                    set_value=set_value_b
+                />
             </div>
         </div>
-        <div
-            style:background-color=move || format!("rgb(0, {}, 0", count_g())
-        >
-            <button // G
-                on:click=move |_| {
-                    set_count_g.update(|n| *n-=1);
-                }
-            >
-                "Less Green"
-            </button>
-            {count_g}
-            <button // G
-                on:click=move |_| {
-                    set_count_g.update(|n| *n+=1);
-                }
-            >
-                "More Green"
-            </button>
-            <progress
-                max="255"
-                value=count_g
-            />
-            <div>
-                <input type="text"
-                    on:input=move |ev| {
-                        set_input_g(event_target_value(&ev));
-                    }
-                >
-                </input>
-                <button
-                    on:click=move |_| {
-                        set_count_g(input_g.get_untracked().parse::<i32>().ok().unwrap_or_default())
-                    }
-                >
-                    "Set Green"
-                </button>
-            </div>
-        </div>
-        <div
-            style:background-color=move || format!("rgb(0, 0, {}", count_b())
-        >
+    }
+}
+
+#[component]
+fn RgbInput(
+    cx: Scope,
+    color: String,
+    value: ReadSignal<i32>,
+    set_value: WriteSignal<i32>,
+) -> impl IntoView {
+    let (input, set_input) = create_signal(cx, "".to_string());
+
+    view! {cx,
+        <div>
             <button // B
                 on:click=move |_| {
-                    set_count_b.update(|n| *n-=1);
+                    set_value.update(|n| *n-=1);
                 }
             >
-                "Less Blue"
+                "Less " {&color}
             </button>
-            //{move || count().get} // on stable version, this would be count.get() instead.
-            {count_b} // this is already a function in nightly Leptos, the closure above is redundant.
-                // count is already a reactive fn, so doing count() here would unwrap the reactivity.
+            //{move || value().get} // on stable version, this would be value.get() instead.
+            {value} // this is already a function in nightly Leptos, the closure above is redundant.
+                // value is already a reactive fn, so doing value() here would unwrap the reactivity.
             <button // B
                 on:click=move |_| {
-                    set_count_b.update(|n| *n+=1);
+                    set_value.update(|n| *n+=1);
                 }
             >
-                "More Blue"
+                "More " {&color}
             </button>
             <progress
                 max="255"
-                value=count_b
+                value=value
             />
             <div>
                 <input type="text"
                     on:input=move |ev| {
-                        set_input_b(event_target_value(&ev));
+                        set_input(event_target_value(&ev));
                     }
                 >
                 </input>
                 <button
                     on:click=move |_| {
-                        set_count_b(input_b.get_untracked().parse::<i32>().ok().unwrap_or_default())
+                        set_value(input.get_untracked().parse::<i32>().ok().unwrap_or_default())
                     }
                 >
-                "Set Blue"
+                    "Set " {&color}
                 </button>
             </div>
-        </div>
         </div>
     }
 }
