22e5c074c2ac171ebf505d3526abff86129eb9a1
Ke7in <kevinwezenter@gmail.com>
2023-05-27 10:55:28 -0400

    finished lessons on variables

---
+++ rustlings/completed_exercises/variables/variables6.rs
@@ -0,0 +1,7 @@
+// variables6.rs
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
+
+const NUMBER: u8 = 3; // const declarations need an explicit type declarations and to be in all CAPS
+fn main() {
+    println!("Number {}", NUMBER);
+}
